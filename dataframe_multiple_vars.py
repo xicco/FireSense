d9ae71145b5ba4186a7b03597cbca3c48b6ec219
Xicco <sammuel.karya@gmail.com>
2025-05-23 16:32:41 +0700

    Switched to multi-var function

--- dataframe_multiple_vars.py
+++ dataframe_multiple_vars.py
@@ -51,6 +51,3 @@ if __name__ == "__main__":
     df = netcdf_to_pandas(path_to_file, ["sm", "sm_uncertainty"])
     print (df.head(15))
     print (df.describe())
-
-# note for nexttime
-# try making thius function also pass in sm_unceratinty, so i can show that in front end
